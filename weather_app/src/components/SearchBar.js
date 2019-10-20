6c1833cca8ac84466979577d329610e370d89692
fausto08 <faustol99@gmail.com>
2019-10-20 18:32:32 -0400

    images load

--- weather_app/src/components/SearchBar.js
+++ weather_app/src/components/SearchBar.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import App from './App'
+
 
 class SearchBar extends React.Component {
   render() {
--- weather_app/src/components/WeatherDisplay.js
+++ weather_app/src/components/WeatherDisplay.js
@@ -1,18 +1,34 @@
 import React from 'react'
-
+import sunny from '../resources/sunny.jfif'
+import thunder from '../resources/thunder.jfif'
+import fog from '../resources/Fog.jfif'
 
 class WeatherDisplay extends React.Component {
   render() {
     return (
       <div>
-        <div className="ui three stackable cards">
+        <div className="ui five stackable cards">
           <div className="card">
             <div className="image">
-              <img src="../resources/sunny.jfif" />
+              <img src={sunny} />
+            </div>
+          </div>
+        
+         
+          <div className="card">
+            <div className="image">
+              <img src={thunder} />
+            </div>
+          </div>
+        
+        
+          <div className="card">
+            <div className="image">
+              <img src={fog} />
             </div>
           </div>
 
-        </div>
+         </div>
       </div>
     )
   }
